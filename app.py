18d76e7bc121b0362e264b8e89069e107ad26f84
U-MangoBook\Mango <kimmg0008@gmail.com>
2015-03-30 00:16:33 +0900

    1.1.0 version

--- app.py
+++ app.py
@@ -18,6 +18,64 @@ DOCX_FOLDER = './temp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOCX_FOLDER'] = DOCX_FOLDER
 
+def find_week(Y, M, D):
+    #Y = id_act_date_Y
+    Y = int(Y)
+    M = int(M)
+    D = int(D)
+    total_day=0
+    total_day = Y * 365 + Y/4 - Y/100 + Y/400
+
+    #M = id_act_date_M-1
+    M = M - 1
+    while M is not 0:
+        if M is 1:
+            total_day += 31
+        if M is 2:
+            total_day += 28
+        if M is 3:
+            total_day += 31
+        if M is 4:
+            total_day += 30
+        if M is 5:
+            total_day += 31
+        if M is 6:
+            total_day += 30
+        if M is 7:
+            total_day += 31
+        if M is 8:
+            total_day += 31
+        if M is 9:
+            total_day += 30
+        if M is 10:
+            total_day += 31
+        if M is 11:
+            total_day += 30
+        if M is 12:
+            total_day += 31
+        M -= 1
+
+    #D = id_act_date_D
+    total_day += D
+
+    week_day = (total_day % 7) - 1
+    if week_day is 0:
+        week_day_char = "일"
+    if week_day is 1:
+        week_day_char = "월"
+    if week_day is 2:
+        week_day_char = "화"
+    if week_day is 3:
+        week_day_char = "수"
+    if week_day is 4:
+        week_day_char = "목"
+    if week_day is 5:
+        week_day_char = "금"
+    if week_day is 6:
+        week_day_char = "토"
+
+    return week_day_char.encode('utf-8')
+
 
 @app.route('/')
 def index():
@@ -30,7 +88,24 @@ def assemble():
         id_king = request.form['KING']
         id_wrt_date = request.form['WRT_DATE']
         id_club_name = request.form['CLUB_NAME']
-        id_act_date = request.form['ACT_DATE']
+        
+#MAKE DATE
+        id_act_date_Y = request.form['ACT_DATE_Y']
+        id_act_date_M = request.form['ACT_DATE_M']
+        id_act_date_D = request.form['ACT_DATE_D']
+        id_act_date = id_act_date_Y + "." + id_act_date_M + "." + id_act_date_D
+
+#FIND WEEK
+#0000-1-1 IS MON(1)
+        week_day_char = find_week(id_act_date_Y, id_act_date_M, id_act_date_D)
+
+#CHECK TIME
+        is_apm = request.form.get('TIME_APM')
+        start_time_h = request.form.get('START_TIME_H')
+        start_time_m = request.form.get('START_TIME_M')
+        end_time_h = request.form.get('END_TIME_H')
+        end_time_m = request.form.get('END_TIME_M')
+
         id_place = request.form['PLACE']
         id_act_conts = request.form['ACTIVITY_CONTENTS']
         check_type = request.form['CHECK_TYPE']
@@ -63,7 +138,6 @@ def assemble():
         check_stu_num=0
         for n in range(0, 33):
             index_value_2 = request.form.getlist('IS_ON'+str(n))
-            print index_value_2
             if index_value_2:
                 act_is_on.append('O')
                 check_stu_num=check_stu_num+1
@@ -83,29 +157,38 @@ def assemble():
 
 
 #COPYING
-        i=0
+        act_day = id_act_date
+        i=1
         today=datetime.date.today()
-        folders=[['form','temp/form_']]
+        folders=[['form','temp/']]
         while 1:
             flag=1
+            directory_name = id_act_date + "(" + week_day_char.decode('utf-8') + ")" + "-" + str(i)
+            print directory_name
+            directory_name_find = (directory_name).encode('utf-8')
+            print directory_name_find
             for b in os.listdir('./temp'):
-                if (b).find(today.strftime("%Y%m%d_"+str(i))) is not -1 :
+                if (b).find(directory_name_find) is not -1 :
                     i=i+1
                     flag=0
                     break
             if flag is 1:
                 break
-        today_date = today.strftime("%Y%m%d_"+str(i))
+        today_date = id_act_date+str(i)
         for src, dst in folders:
-            shutil.copytree(src,dst+today_date)
+            shutil.copytree(src,dst+directory_name)
         
 #MODIFYING
-        wordFile = open('temp/form_'+today_date+'/word/document.xml', 'r')
+        print week_day_char
+        directory_name = id_act_date+"(" + week_day_char + ")"+"-"+str(i)
+        wordFile = open('temp/'+directory_name+'/word/document.xml', 'r')
         print wordFile
         line = wordFile.read()
+        write_date = id_act_date +"(" + week_day_char + ")" + " " + is_apm + " " 
+        write_date += start_time_h + ":" + start_time_m + "~" + end_time_h + ":" + end_time_m
 
         t1 = line.replace('$KING$', id_king).replace('$WRT_DATE$', id_wrt_date).replace('$CLUB_NAME$', id_club_name)
-        t2 = t1.replace('$ACT_DATE$', id_act_date).replace('$PLACE$', id_place).replace('$ACTIVITY_CONTENTS$', id_act_conts)
+        t2 = t1.replace('$ACT_DATE$', write_date).replace('$PLACE$', id_place).replace('$ACTIVITY_CONTENTS$', id_act_conts)
         
         total_num=0
         for m in range(0, 33):
@@ -132,26 +215,26 @@ def assemble():
         t2 = t2.replace('$TYPE$', str(check_type))
 
         wordFile.close()
-        writeFile = open('temp/form_'+today_date+'/word/document.xml', 'w')
+        writeFile = open('temp/'+directory_name+'/word/document.xml', 'w')
         writeFile.write(t2)
         writeFile.close()
 #PUT_IMAGE
-        os.unlink('temp/form_'+today_date+'/word/media/image1.jpeg')
-        os.rename('image/'+filename_img1, 'temp/form_'+today_date+'/word/media/image1.jpeg')
-        os.unlink('temp/form_'+today_date+'/word/media/image2.jpeg')
-        os.rename('image/'+filename_img2, 'temp/form_'+today_date+'/word/media/image2.jpeg')
+        os.unlink('temp/'+directory_name+'/word/media/image1.jpeg')
+        os.rename('image/'+filename_img1, 'temp/'+directory_name+'/word/media/image1.jpeg')
+        os.unlink('temp/'+directory_name+'/word/media/image2.jpeg')
+        os.rename('image/'+filename_img2, 'temp/'+directory_name+'/word/media/image2.jpeg')
 
 #RETURN
-        return render_template('draw.html', PIC_NAME='../temp/form_'+today_date+'/word/media/image2.jpeg', TODAY=today_date)
+        return render_template('draw.html', PIC_NAME='../temp/'+directory_name+'/word/media/image2.jpeg', TODAY=directory_name)
         #return render_template('check.html', KING=id_king, WRT_DATE=id_wrt_date,
         #    CLUB_NAME=id_club_name, ACT_DATE=id_act_date, PLACE=id_place,
-        #     ACTIVITY_CONTENTS=id_act_conts, FILE_NAME='form_'+today_date+'.docx')
+        #     ACTIVITY_CONTENTS=id_act_conts, FILE_NAME=''+directory_name+'.docx')
 
 
 @app.route('/complete/', methods=['GET','POST'])
 def complete():
     if request.method=='POST':
-        today_date = request.form['today_date']
+        directory_name = request.form['today_date']
         image_file = request.form['hidden_file']
         file = open("./image/image2.jpeg", 'wb')
         target_decode = image_file.split(",")[1]
@@ -159,14 +242,14 @@ def complete():
 
         file.close()
         image_name="image2.jpeg"
-        os.unlink('temp/form_'+today_date+'/word/media/image2.jpeg')
-        os.rename('image/'+image_name, 'temp/form_'+today_date+'/word/media/image2.jpeg')
+        os.unlink('temp/'+directory_name+'/word/media/image2.jpeg')
+        os.rename('image/'+image_name, 'temp/'+directory_name+'/word/media/image2.jpeg')
 
     #ZIP FILE
-        shutil.make_archive("temp/form_"+today_date, "zip", "temp/form_"+today_date)
-        os.rename("temp/form_"+today_date+".zip", "temp/form_"+today_date+".docx")
+        shutil.make_archive("temp/"+directory_name, "zip", "temp/"+directory_name)
+        os.rename("temp/"+directory_name+".zip", "temp/"+directory_name+".docx")
 
-        return render_template('complete.html', FILE_NAME='form_'+today_date+'.docx')
+        return render_template('complete.html', FILE_NAME=''+directory_name+'.docx')
 
 
 @app.route('/temp/<date>/word/media/<filename>')
